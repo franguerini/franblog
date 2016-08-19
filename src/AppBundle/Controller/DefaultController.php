a841af73488c7b430d0701dd6ee7b3d928df4cfc
Nacho Capodanno <icapodanno@flowcode.com.ar>
2016-08-19 17:57:04 -0300

    empiezo con seguridad}

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,26 +13,39 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        var_dump($request->get('category'));
+
+        $filteredPost = array();
         $filter = $request->get('category');
+        $filterAuthor = $request->get('autores');
 
         $em = $this->getDoctrine()->getManager();
+
 
         $posts = $em->getRepository('AppBundle:Post')->findAll();
         $categorias = $em->getRepository('AppBundle:Categoria')->findAll();
         $autores = $em->getRepository('AppBundle:Autor')->findAll();
 
-        $filteredPost = array();
+        var_dump($filter);
+
+        var_dump($filterAuthor);
+
 
         foreach ($posts as $post){
-            if (in_array($post->getCategoria(), $filter)){
-                array_push($filteredPost, $post);
+            if($filter != null && $filterAuthor != null) {
+
+                if (in_array($post->getCategoria()->getId(), $filter)  && in_array($post->getAutor()->getId(), $filterAuthor)){
+                    array_push($filteredPost, $post);
+                }
+            }
+            else {
+                $filteredPost = $posts;
             }
         }
 
         return $this->render('default/index.html.twig', [
-            'posts' => $filteredPost, 'categorias' => $categorias, 'autores' => $autores,
+            'posts' => $filteredPost, 'categorias' => $categorias, 'autores' => $autores, 'filtroCategorias' => $filter, 'filtroAutor' => $filterAuthor
         ]);
+
     }
 
 
